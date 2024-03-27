b067147273418528f348523523a2cb26a26030b9
Lukas Jerusalem <lukasjerusalem@web.de>
2024-03-27 20:12:32 +0000

    no error message by cicking beneath image

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const headings = document.querySelectorAll('.subheading');
 
 headings.forEach(item => {
-  item.addEventListener("click", function handleClick(event) {
+  item.children[1].addEventListener("click", function handleClick(event) {
     if (event.target.getAttribute('src') == "assets/images/icon-minus.svg") {
       event.target.src="assets/images/icon-plus.svg";
       event.target.parentElement.style.border ="";
